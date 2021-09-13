31e692812570b479f9dd11a3e06104b3892fa36e
Karlson Drendel <kdrendel99@gmail.com>
2021-09-12 20:30:48 -0700

    Center header, add Styled Components library

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import styled from 'styled-components';
+
+const DiscussionBoardHeader = styled.h1`
+  font-size: 24px;
+  text-align: center;
+  color: black;
+`;
+
+const CenterText = styled.section`
+  text-align: center;
+`;
 
 function Header(){
   return(
     <React.Fragment>
       <div style={{marginLeft: "30px"}}>
-        <h1>Discussion Forum</h1>
-        <p>A discussion forum application</p>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/signin">Sign in</Link>
-          </li>
-        </ul>
+        <DiscussionBoardHeader>
+          <h1>Discussion Forum</h1>
+        </DiscussionBoardHeader>
+        <CenterText>
+          <p>A forum to pick your brain.</p>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/signin">Sign in</Link>
+            </li>
+          </ul>
+        </CenterText>
       </div>
       <hr/>
     </React.Fragment>
--- src/components/Post.js
+++ src/components/Post.js
@@ -7,10 +7,10 @@ function Post(props){
       <div onClick = {() => props.whenPostClicked(props.id)}>
         <h1>{props.title}</h1>
         <h3>{props.body}</h3>
-        <h4>{props.score}</h4>
+        <h4>Score: {props.score}</h4>
       </div>
       <button onClick = {() => props.whenVoteClicked(props.id, props.score, 1)}>Upvote</button>
-      <button onClick = {() => props.whenVoteClicked(props.id, props.score, -1)}>Upvote</button>
+      <button onClick = {() => props.whenVoteClicked(props.id, props.score, -1)}>Downvote</button>
       {/* <button onClick = {() => props.whenVoteClicked(props.id, 1)}>Upvote in Post</button>  */}
       {/* <button onClick = {() => props.whenVoteClicked(props.id, -1)}>Down vote in post</button>  */}
       <hr/>
